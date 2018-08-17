eae2c3638ee10e0bb9a89558a03d2bd2138831bf
Junwu <978162622@qq.com>
2018-08-18 02:54:20 +0800

    完成模糊查询以及编辑功能

--- route/studentRouter.js
+++ route/studentRouter.js
@@ -3,16 +3,28 @@ let router = express.Router()
 const path = require('path')
 const helper = require('../tools/helper')
 const template = require('art-template')
+const querystring = require('querystring')
+
+router.use((request, response, next) => {
+    if (!request.url != '/logout') {
+        if (!request.session.username)
+            helper.tips(response, '请先登录！', '/manage/login')
+        else
+            next()
+    } else {
+        next()
+    }
+})
 
 router.get('/index', (request, response) => {
-    if(!request.session.username){
-        helper.tips(response, '请先登录！', '/manage/login')
-    }else {
+    let obj = request.query.search ? { studentname: { $regex: request.query.search } } : {}
+    helper.find('student', obj, (result) => {
         let html = template(path.join(__dirname, '../template/index.html'), {
+            data: result,
             username: request.session.username
         })
         response.send(html)
-    }
+    })
 })
 
 router.get('/logout', (request, response) => {
@@ -21,14 +33,54 @@ router.get('/logout', (request, response) => {
 })
 
 router.get('/add', (request, response) => {
-    if(!request.session.username){
+    if (!request.session.username) {
         helper.tips(response, '请先登录！', '/manage/login')
-    }else {
+    } else {
         let html = template(path.join(__dirname, '../template/add.html'), {
             username: request.session.username
         })
         response.send(html)
     }
+})
+
+router.post('/add', (request, response) => {
+    helper.insertOne('student', request.body, (result) => {
+        if (request.body.username) helper.tips(response, '添加失败！', '/student/add')
+        else helper.tips(response, '添加完成！', '/student/index')
+    })
+})
+
+router.get('/delete/:id', (request, response) => {
+    let _id = request.params.id
+    helper.deleteOne('student', {
+        _id: helper.ObjectId(_id)
+    }, (result) => {
+        if(result.length != 0) {
+            helper.tips(response, '删除成功！', '/student/index')
+        }else {
+            response.redirect('/student/index')
+        }
+    })
+})
+
+router.get('/edit/:id', (request, response) => {
+    helper.find('student', { _id: helper.ObjectId(request.params.id) }, (result) => {
+        if(result.length != 0){
+            let html = template(path.join(__dirname, '../template/edit.html'), {
+                data: result[0],
+                username: request.session.username
+            })
+            response.send(html)
+        }else {
+            helper.tips(response, '没有查询到该条数据，请联系管理员！', '/student/index')
+        }
+    })
+})
+
+router.post('/edit/:id', (request, response) => {
+    helper.updateOne('student', { _id: helper.ObjectId( request.params.id ) }, request.body, (result) => {
+        response.redirect('/student/index')
+    })
 })
 
 module.exports = router
--- tools/helper.js
+++ tools/helper.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectID
 
 // Connection URL
 const url = 'mongodb://localhost:27017'
@@ -9,7 +10,9 @@ const dbName = 'managerDB'
 module.exports = {
     find(collectionName, obj, callback) {
         // Use connect method to connect to the server
-        MongoClient.connect(url, { useNewUrlParser:true }, function (err, client) {
+        MongoClient.connect(url, {
+            useNewUrlParser: true
+        }, function (err, client) {
             if (err) throw err
             console.log("Connected successfully to server");
 
@@ -24,7 +27,9 @@ module.exports = {
     },
     insertOne(collectionName, obj, callback) {
         // Use connect method to connect to the server
-        MongoClient.connect(url, { useNewUrlParser:true }, function (err, client) {
+        MongoClient.connect(url, {
+            useNewUrlParser: true
+        }, function (err, client) {
             if (err) throw err
             console.log("Connected successfully to server");
 
@@ -37,7 +42,42 @@ module.exports = {
             });
         })
     },
+    deleteOne(collectionName, obj, callback) {
+        MongoClient.connect(url, {
+            useNewUrlParser: true
+        }, function (err, client) {
+            if (err) throw err
+            console.log("Connected successfully to server");
+
+            const db = client.db(dbName);
+
+            db.collection(collectionName).deleteOne(obj, (err, result) => {
+                if (err) throw err
+                client.close()
+                callback(result)
+            });
+        })
+    },
+    updateOne(collectionName, filterObj, obj, callback) {
+        MongoClient.connect(url, {
+            useNewUrlParser: true
+        }, function (err, client) {
+            if (err) throw err
+            console.log("Connected successfully to server");
+
+            const db = client.db(dbName);
+
+            db.collection(collectionName).updateOne(filterObj, {
+                $set: obj
+            }, (err, result) => {
+                if(err) throw err
+                client.close()
+                callback(result);
+            });
+        })
+    },
     tips(response, message, url) {
         response.send(`<script>window.alert('${ message }');window.location='${ url }'</script>`)
-    }
+    },
+    ObjectId
 }
